194b3c75aa28c760164afbc155b677217bae3dbb
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2024-02-14 01:25:21 -0500

    Agregar archivos de proyecto.

---
+++ ValentinsCard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ValentinsCard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
